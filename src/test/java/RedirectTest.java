ae63ddc81eb4362dbcb8e8cd5cb924a444555749
ruakzor <ol.v.koz@yandex.com>
2022-11-15 00:05:28 +0500

    Ex7 longRedirectTest

--- src/test/java/RedirectTest.java
+++ src/test/java/RedirectTest.java
@@ -1,13 +1,12 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
 import org.junit.jupiter.api.Test;
-import io.restassured.http.Headers;
+
 
 public class RedirectTest {
 
     @Test
-    public void testHelloWorld(){
+    public void testRedirect(){
         Response response = RestAssured
                 .given()
                 .redirects()
@@ -21,6 +20,39 @@ public class RedirectTest {
         String locationHeaders = response.getHeader("Location");
         System.out.println(locationHeaders);
 
+
+    }
+
+    @Test
+    public void testLongRedirect(){
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String redirectURL =  response.getHeader("Location");;
+        int statusCode = response.getStatusCode();
+
+        while (statusCode == 301 ) {
+            Response resp = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(redirectURL)
+                    .andReturn();
+
+            redirectURL = resp.getHeader("Location");
+            statusCode = resp.getStatusCode();
+
+        };
+
+        System.out.println(statusCode);
+        System.out.println(redirectURL);
 
     }
 }
