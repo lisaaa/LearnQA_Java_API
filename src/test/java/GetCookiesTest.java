4e4e3a49150f49fd9dc2f500bb30a38b3891b972
ruakzor <ol.v.koz@yandex.com>
2022-11-20 23:01:18 +0500

    Ex11 cookies test

---
+++ src/test/java/GetCookiesTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GetCookiesTest {
+
+    @Test
+    public void checkCookieResponse(){
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        Map<String, String> cookies = new HashMap<>();
+        cookies = response.getCookies();
+        System.out.println(cookies);
+        assertTrue("Response has 'HomeWork' cookie", cookies.containsKey("HomeWork"));
+    }
+}
