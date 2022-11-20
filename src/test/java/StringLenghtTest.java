536f778a992537bb3097a3b898402339969ce211
ruakzor <ol.v.koz@yandex.com>
2022-11-20 22:44:27 +0500

    Ex10 Short text test

---
+++ src/test/java/StringLenghtTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.Assert.assertTrue;
+
+public class StringLenghtTest {
+
+    public static final int LENGHT = 15;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"меньше15","длинабольше15символов"})
+    public void checkStringlenght(String text){
+
+        assertTrue("В этом тексте больше 15 сиволов",text.length() > LENGHT);
+
+    }
+}
